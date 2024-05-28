1ca052ca8f08eccd7b1505d96d887023228c4444
Metrinenaliaka <metrinenam@gmail.com>
2024-05-28 17:02:34 +0300

    babel initialization

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+instatiating Babel
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+if __name__ == "__main__":
+    app.run()
