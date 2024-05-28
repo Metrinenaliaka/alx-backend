6b5c81301f5f87cdfe5824b9922f66ba1b9d4311
Metrinenaliaka <metrinenam@gmail.com>
2024-05-28 16:49:22 +0300

    simple flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+a simple flask app
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    """
+    render a html file
+    """
+    return render_template('0-index.html')
